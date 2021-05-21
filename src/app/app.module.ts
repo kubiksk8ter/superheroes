4d9ce50b3f765bff0f59edd60fe9fa24a937b840
Jakub Holeček <71323057+kubiksk8ter@users.noreply.github.com>
2021-05-21 11:30:53 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { SuperheroesComponent } from './superheroes/superheroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuperheroesComponent
   ],
   imports: [
     BrowserModule,
