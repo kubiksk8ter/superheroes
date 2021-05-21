d3de9f8ec12e6541b39ab6d34d8a5607e80e1233
Jakub Holeček <71323057+kubiksk8ter@users.noreply.github.com>
2021-05-21 11:31:17 +0200

    Create db.js

---
+++ src/server/db.js
@@ -0,0 +1,18 @@
+const superheroes = [{
+   id: 1,
+   firstName: 'Steve',
+   lastName: 'Rogers',
+   dateOfBirth: '4.7.1918',
+   superPowers: 'Fast, Strong'
+},{
+   id: 2,
+   firstName: 'Tony',
+   lastName: 'Stark',
+   dateOfBirth: '29.5.1970',
+   superPowers: 'Genius, exosuit' 
+}];
+
+module.exports = {
+   superheroes
+};  
+
