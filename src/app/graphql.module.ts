d79d0d6504836abed8b94919796df388127ef8ca
Jakub Holeček <71323057+kubiksk8ter@users.noreply.github.com>
2021-08-22 21:29:13 +0200

    Update graphql.module.ts

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import {APOLLO_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache, defaultDataIdFromObject} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 
-const uri = 'https://superheroes.jakubholecek.cz:2000/graphql'; // <-- add the URL of the GraphQL server here
+const uri = 'https://...'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
